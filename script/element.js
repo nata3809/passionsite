b5c9546861a0923bdfd388f0ff907ce486be047d
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-02-20 13:30:38 +0100

    elementside nu med data

---
+++ script/element.js
@@ -0,0 +1,32 @@
+// lav url search objekt
+const urlParams = new URLSearchParams(window.location.search);
+// find id
+console.log(urlParams);
+
+const id = urlParams.get("id");
+const url = `https://passion-172c.restdb.io/rest/filmliste/${id}`;
+
+const options = {
+  headers: {
+    "x-apikey": "63ef4921478852088da683c2",
+  },
+};
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  vis(json);
+}
+
+function vis(movie) {
+  console.log(movie);
+  document.querySelector(".genre").textContent = movie.genre;
+  document.querySelector(".filmtitel_bread").textContent = movie.titel;
+  document.querySelector(".element_grid1-2 img").src = movie.billede;
+  document.querySelector(".filmtitel").textContent = movie.titel;
+  document.querySelector(".year").textContent = movie.year;
+  document.querySelector(".time").textContent = movie.varighed;
+  document.querySelector(".rating").textContent = movie.ratings + " STJERNER";
+  document.querySelector(".filmbeskrivelse").innerHTML = movie.beskrivelse;
+}
+
+hentData();
