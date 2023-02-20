20deb498886e9f49bc4fab72b2eaa4be970a1bb0
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-02-20 11:42:42 +0100

    dataarray tilføjet + kosmetiske ændringer

---
+++ script/genre_v2.js
@@ -0,0 +1,28 @@
+const url = "https://passion-172c.restdb.io/rest/filmliste";
+
+const options = {
+  headers: {
+    "x-apikey": "63ef4921478852088da683c2"
+  }
+};
+
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  vis(json);
+}
+
+const movielist = document.querySelector(".movielist");
+const template = document.querySelector(".movie_template").content;
+
+function vis(json) {
+  console.log(json);
+
+  json.forEach((movie) => {
+    const klon = template.cloneNode(true);
+    klon.querySelector("img").src = movie.billede;
+    movielist.appendChild(klon);
+  });
+}
+
+hentData();
