27ae6f1ce2759a25b1f2354286de30f019574cff
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-02-21 11:21:47 +0100

    filtrering

--- script/genre_v2.js
+++ script/genre_v2.js
@@ -2,28 +2,50 @@ const url = "https://passion-172c.restdb.io/rest/filmliste";
 
 const options = {
   headers: {
-    "x-apikey": "63ef4921478852088da683c2"
-  }
+    "x-apikey": "63ef4921478852088da683c2",
+  },
 };
 
-async function hentData() {
-  const respons = await fetch(url, options);
-  const json = await respons.json();
-  vis(json);
-}
+let data = [];
+let filter = "alle";
 
 const movielist = document.querySelector(".movielist");
 const template = document.querySelector(".movie_template").content;
 
-function vis(json) {
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  data = json;
+  vis();
+}
+
+document.querySelector("#genre").addEventListener("change", vis);
+
+function vis() {
+  movielist.textContent = "";
+  if (this.value) {
+    console.log(this.value);
+    filter = this.value;
+  }
+  data.forEach((movie) => {
+    if (movie.genre == filter || filter == "alle") {
+      const klon = template.cloneNode(true);
+      klon.querySelector("img").src = movie.billede;
+      klon.querySelector("a").href = "element.html?id=" + movie._id;
+      movielist.appendChild(klon);
+    }
+  });
+}
+
+/* function vis(json) {
   console.log(json);
 
   json.forEach((movie) => {
     const klon = template.cloneNode(true);
     klon.querySelector("img").src = movie.billede;
-    klon.querySelector("a").href = "element.html?id=" + movie._id
+    klon.querySelector("a").href = "element.html?id=" + movie._id;
     movielist.appendChild(klon);
   });
 }
-
+ */
 hentData();
