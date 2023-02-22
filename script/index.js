003ad2b5d7a90b4c5483c38a1fc59c50943a0477
Natasja Nielsen <Nata3809@stud.kea.dk>
2023-02-22 10:31:14 +0100

    INDEX

--- script/index.js
+++ script/index.js
@@ -2,8 +2,8 @@ const url = "https://passion-172c.restdb.io/rest/filmliste?max=5";
 
 const options = {
   headers: {
-    "x-apikey": "63ef4921478852088da683c2"
-  }
+    "x-apikey": "63ef4921478852088da683c2",
+  },
 };
 
 async function hentData() {
@@ -21,9 +21,33 @@ function vis(json) {
   json.forEach((movie) => {
     const klon = template.cloneNode(true);
     klon.querySelector("img").src = movie.billede;
-    klon.querySelector("a").href = "element.html?id=" + movie._id
+    klon.querySelector("a").href = "element.html?id=" + movie._id;
     movielist.appendChild(klon);
   });
 }
 
 hentData();
+
+const url2 = `https://passion-172c.restdb.io/rest/filmliste/63ea210daa860750000545bc`;
+
+const options2 = {
+  headers: {
+    "x-apikey": "63ef4921478852088da683c2",
+  },
+};
+async function hentData2() {
+  const respons = await fetch(url2, options2);
+  const json = await respons.json();
+  console.log(json);
+  vis2(json);
+}
+
+function vis2(movie) {
+  console.log(movie);
+  document.querySelector(".udvalgt_film_knap").href = "element.html?id=" + movie._id;
+  document.querySelector(".udvalgt_film_img").src = movie.billede;
+  document.querySelector(".udvalgte_film_titel").textContent = movie.titel;
+  document.querySelector(".udvalgte_film_text").innerHTML = movie.beskrivelse;
+}
+
+hentData2();
